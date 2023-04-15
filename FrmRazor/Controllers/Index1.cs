f2c0609619aa5a77bb44b17bb73de5dd1c131bee
hasol <henry.solito@catolica.edu.sv>
2023-04-15 16:50:58 -0600

    Add project files.

---
+++ FrmRazor/Controllers/Index1.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace FrmRazor.Controllers
+{
+    public class Index1 : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}
